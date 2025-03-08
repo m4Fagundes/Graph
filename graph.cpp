313a553bb8549cebefc9abfd216276daf424d81a
Matheus Fagundes <124789617+m4Fagundes@users.noreply.github.com>
2025-03-08 10:40:21 -0300

    fizing the graph

--- graph.cpp
+++ graph.cpp
@@ -159,7 +159,7 @@ int main() {
 
     graph.addEdge(0, 1);
     graph.addEdge(0, 3);
-    graph.addEdge(0, 5);
+    graph.addEdge(0, 4);
     graph.addEdge(1, 2);
     graph.addEdge(1, 3);
     graph.addEdge(1, 4);
@@ -167,6 +167,7 @@ int main() {
     graph.addEdge(2, 5);
     graph.addEdge(2, 3);
     graph.addEdge(3, 5);
+    graph.addEdge(4, 5);
 
     cout << "BFS starting from node 0: ";
     graph.bfs(0);
